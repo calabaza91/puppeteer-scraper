2cace14f91bb101e51c25cfd00804ecd6464589d
Caleb Cox <calebcox2010@gmail.com>
2021-12-21 00:05:30 -0600

    Setup code
    node_modules folder not added to repo***

---
+++ config.js
@@ -0,0 +1,4 @@
+// contains site to scrape
+module.exports = {
+    url: 'https://books.toscrape.com/catalogue/the-four-agreements-a-practical-guide-to-personal-freedom_970/index.html'
+}
---
+++ scraper.js
@@ -0,0 +1,20 @@
+/**
+ * Web Scraper for books
+ */
+
+//Requirements
+const puppeteer = require('puppeteer')
+const { url } = require('./config')
+
+;
+(async() => {
+    // Open Browser
+
+    // Setup Browser
+
+    // Close Browser
+
+})().catch(error => {
+    console.log(error)
+    process.exit(1)
+})
