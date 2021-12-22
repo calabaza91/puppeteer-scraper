e6c30d658404a2214e01bc629c48fafa250e047d
Caleb Cox <calebcox2010@gmail.com>
2021-12-21 20:19:56 -0600

    Update scraper.js - get data from bookstore
    Add name and price selectors
    Go to bookstore, grab and return innerHTML values of name and price

--- scraper.js
+++ scraper.js
@@ -12,11 +12,10 @@ const { url } = require('./config')
 
     // Open Browser
     const browser = await puppeteer.launch({
-        headless: false,
+        headless: true,
         slowMo: 250,
         devtools: false
     })
-
     const page = await browser.newPage()
 
 
@@ -24,6 +23,20 @@ const { url } = require('./config')
     await page.setDefaultTimeout(10000)
     await page.setViewport({ width: 1200, height: 800 })
     await page.setUserAgent(randomUserAgent.getRandom()) // Make scraper undetectable
+
+
+    // Get data from bookstore
+    const nameSelector = '.product_main h1'
+    const priceSelector = '.price_color'
+    await page.goto(url)
+    await page.waitForSelector(nameSelector)
+    await page.waitForSelector(priceSelector)
+    const name = await page.$eval(nameSelector, e => e.innerHTML)
+    const price = await page.$eval(priceSelector, e => e.innerHTML)
+    const nameTrim = name.trim()
+    const priceTrim = price.trim()
+
+    console.log('Name: ' + nameTrim + '\nPrice: ' + priceTrim)
 
     // Close Browser
     await browser.close()
