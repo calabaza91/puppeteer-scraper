ccd835d36da9b5158c24827e71d2e9a8d57a5712
Caleb Cox <calebcox2010@gmail.com>
2021-12-21 19:49:19 -0600

    Finish setup
    Add code to open and close browser.
    Setup default timeout and viewport size.

--- scraper.js
+++ scraper.js
@@ -8,11 +8,23 @@ const { url } = require('./config')
 
 ;
 (async() => {
+
     // Open Browser
+    const browser = await puppeteer.launch({
+        headless: false,
+        slowMo: 250,
+        devtools: false
+    })
+
+    const page = await browser.newPage()
+
 
     // Setup Browser
+    await page.setDefaultTimeout(10000)
+    await page.setViewport({ width: 1200, height: 800 })
 
     // Close Browser
+    await browser.close()
 
 })().catch(error => {
     console.log(error)
